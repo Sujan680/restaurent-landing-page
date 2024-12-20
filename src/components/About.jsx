121824ce6a6310de3b0606431dd3052941bc9d91
sujan <budhasujan3@gmail.com>
2024-12-20 19:43:24 +0545

    About section of the restaurent app

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <h2 className="mb-3 text-center text-3xl tracking-tighter lg:text-4xl">
         About Us
       </h2>
-      <div className="mb-8 mx-auto h-2 w-36  bg-rose-300 lg:-rotate-3"></div>
+      <div className="mb-10 mx-auto h-2 w-36  bg-rose-300 lg:-rotate-3"></div>
       <div className="flex  flex-wrap ">
         <div className="w-full p-4 lg:w-1/2">
           <img
